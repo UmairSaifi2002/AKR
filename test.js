70f5d2e657c6ee34f8b8e531f8e93ef122bae69e
Umair Saifi <umairsaifi2002@gmail.com>
2024-08-17 00:07:20 +0530

    New Commit

--- test.js
+++ test.js
@@ -570,3 +570,77 @@ document.addEventListener("DOMContentLoaded", () => {
   //   "max-glare": 1,
   // });
 });
+
+
+// for light theme
+
+// test.js
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   const icon = document.getElementById("ICON");
+
+//   icon.addEventListener("click", function () {
+//     const body = document.body;
+    
+//     // Toggle between light and dark themes
+//     if (body.classList.contains("light-theme")) {
+//       body.classList.remove("light-theme");
+//       body.classList.add("dark-theme");
+      
+//       // Change the icon to sun
+//       icon.src = "moon.png"; // Make sure you have a moon.png for dark mode
+//     } else {
+//       body.classList.remove("dark-theme");
+//       body.classList.add("light-theme");
+
+//       //
+      
+//       // Change the icon to moon
+//       icon.src = "sun.png"; // Change back to sun for light mode
+//     }
+//   });
+// });
+
+// test.js
+
+document.addEventListener("DOMContentLoaded", function () {
+  let icon = document.getElementById("ICON");
+  let m = document.querySelector('.video-bg-back');
+  let logo = document.querySelector('.logo');
+  let n = document.querySelectorAll('.colora');
+  let c = document.querySelector('.contact');
+
+  icon.addEventListener("click", function () {
+    const body = document.body;
+
+    // Toggle between light and dark themes
+    if (body.classList.contains("light-theme")) {
+      body.classList.remove("light-theme");
+      body.classList.add("dark-theme");
+
+      // Change the icon to moon for dark mode
+      icon.src = "sun.png"; 
+      logo.src = "The Akr Letter White PNG.png";
+      c.style.backgroundColor = '#222'
+      n.forEach(e => {
+        e.style.color = "white";
+      });
+      m.innerHTML = `<source class="video-back" src="dark-theme-video.mp4" type="video/mp4">`;
+      m.load();
+
+    } else {
+      body.classList.remove("dark-theme");
+      body.classList.add("light-theme");
+
+      // Change the icon back to sun for light mode
+      icon.src = "moon.png"; 
+      logo.src = "The AKR Letter  Black PNG.png";
+      c.style.backgroundColor = '#22222275'
+      n.forEach(e => {
+        e.style.color = "black";
+      });
+      m.innerHTML = `<source class="video-back" src="light-theme-video.mp4" type="video/mp4">`;
+      m.load();
+    }
+  });
+});
